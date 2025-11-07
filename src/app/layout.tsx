a7018c5774440c45ada0ed0db56dabd986042751
jfernandes5 <jfernandes@tasken.com>
2025-11-07 14:43:31 -0300

    feat: no translation and pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import { Geist } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "../app/globals.css";
 import { Providers } from "./providers";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
   weight: ["400", "700"],
 });
-
-import { JetBrains_Mono } from "next/font/google";
 
 const geistMono = JetBrains_Mono({
   variable: "--font-geist-mono",
@@ -18,8 +18,22 @@ const geistMono = JetBrains_Mono({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background w-full h-full text-foreground`}>
+    <html lang="pt-BR" translate="no" suppressHydrationWarning>
+      <head>
+        <meta name="google" content="notranslate" />
+        <meta name="theme-color" content="#0f172a" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-512.png" />
+        <link rel="icon" href="/icons/icon-192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/icons/icon-512.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Meu Sistema" />
+      </head>
+
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background w-full h-full text-foreground`}
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
